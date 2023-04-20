38ca11991ebf1eeab3fbcd3ccd37a2d5c1dc1f10
IBOUU03 <118839500+IBOUU03@users.noreply.github.com>
2023-04-20 14:04:14 +0400

    Add files via upload

---
+++ Task3/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Task3.Models;
+
+namespace Task3.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Feature> Features { get; set; }
+    }
+}
