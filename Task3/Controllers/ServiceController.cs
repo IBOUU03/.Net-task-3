2e53ea26caa5c70e802dcd03adda4e6430d0452b
IBOUU03 <118839500+IBOUU03@users.noreply.github.com>
2023-04-20 14:02:48 +0400

    Add files via upload

---
+++ Task3/Controllers/ServiceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task3.DataAccess;
+using Task3.Models;
+using Task3.ViewModels;
+
+namespace Task3.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            ServiceViewModel svm = new ServiceViewModel { 
+                SerFeatures = Data.SerFeatures
+            };
+            return View(svm);
+        }
+
+        public IActionResult Deatil(int id)
+        {
+            SerFeature serFeature = Data.SerFeatures.Find(x => x.Id == id);
+
+            return View(serFeature);
+        }
+        
+    }
+}
